a3fa196f4a09c2260fb57e410d49b9a126865b28
Sergio Carlos Orozco Torres <scot3004@gmail.com>
2019-05-17 17:18:22 -0500

    feat(headless): Headless config for tests

---
+++ protractor/headless.config.ts
@@ -0,0 +1,17 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/home.spec.js' ],
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
+};
