581fbdc76ab5c82e32a2bf14f6c17f22fffb09a2
Sergio Carlos Orozco Torres <scot3004@gmail.com>
2019-06-13 09:39:28 -0500

    refactor(waits): Esperas de cargas de pagina y Jasmine

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,8 +3,11 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/**.spec.js'],
-  getPageTimeout: 2000,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 30000,
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
